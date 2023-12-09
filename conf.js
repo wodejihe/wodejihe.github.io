6b95ecc62a8767037cf3057d3178a98c448114e4
wodejihe <118356419+wodejihe@users.noreply.github.com>
2023-12-09 21:57:21 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -79,7 +79,9 @@ jsproxy_config({
   /**
    * 自定义注入页面的 HTML
    */
-  inject_html: '<!-- custom html -->',
+  inject_html: '<p style="text-align: center;">
+    <span style="font-size: 20px;"><strong><span style="color: #C0504D;">testing</span></strong></span>
+</p>',
 
   /**
    * URL 自定义处理（设计中）
