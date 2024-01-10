c30c02871c84ca9f355f4ec51236ded4be827a86
wodejihe <118356419+wodejihe@users.noreply.github.com>
2024-01-10 15:55:30 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '110',
+  ver: '113',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -39,13 +39,11 @@ jsproxy_config({
       lines: {
         // 收费版（高权重）
         //'node-cfworker-2.etherdream.com': 4,
-
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        //'b.007.workers.dev': 1,
-        //'b.hehe.workers.dev': 1,
-        //'b.lulu.workers.dev': 1,
-        //'b.jsproxy.workers.dev': 1,
+        'b.007.workers.dev': 1,
+        'b.lulu.workers.dev': 1,
+        'b.jsproxy.workers.dev': 1,
       }
     }
   },
@@ -85,13 +83,96 @@ jsproxy_config({
    * URL 自定义处理（设计中）
    */
   url_handler: {
-    'https://www.baidu.com/img/baidu_resultlogo@2.png': {
-      replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
+        'https://t66y.com/': {
+      replace: 'https://baidu.com'
     },
     'https://www.pornhub.com/': {
       redir: 'https://php.net/'
     },
-    'http://haha.com/': {
+    'https://www.tumblr.com/': {
+      content: 'Hello World'
+    },
+    
+    'https://google.com/': {
+      content: 'Hello World'
+    },
+    'https://www.google.com/': {
+      content: 'Hello World'
+    },
+    'https://youtube.com/': {
+      content: 'Hello World'
+    },
+    'https://www.youtube.com/': {
+      content: 'Hello World'
+    },
+    'https://www.twitter.com/': {
+      content: 'Hello World'
+    },
+    'https://facebook.com/': {
+      content: 'Hello World'
+    },
+    'https://www.facebook.com/': {
+      content: 'Hello World'
+    },
+    'https://twitter.com/': {
+      content: 'Hello World'
+    },
+    'https://www.instagram.com/': {
+      content: 'Hello World'
+    },
+    'https://instagram.com/': {
+      content: 'Hello World'
+    },
+    
+    'https://zh.xhamster.com/': {
+      content: 'Hello World'
+    },
+    'https://www.xhamster.com/': {
+      content: 'Hello World'
+    },
+    'https://www.xnxx.com/': {
+      content: 'Hello World'
+    },
+    'https://www.xvideos.com/': {
+      content: 'Hello World'
+    },
+    'https://www.pornhub.com/': {
+      content: 'Hello World'
+    },
+    'https://www.onlyfans.com/': {
+      content: 'Hello World'
+    },
+    'https://onlyfans.com/': {
+      content: 'Hello World'
+    },
+    'https://www.youporn.com/': {
+      content: 'Hello World'
+    },
+    'https://youporn.com/': {
+      content: 'Hello World'
+    },
+    'https://www.pixiv.net/': {
+      content: 'Hello World'
+    },
+        'https://wallhaven.cc/w/vqylel': {
+      content: 'Hello World'
+    },
+    'https://w.wallhaven.cc/full/vq/wallhaven-vqylel.jpg': {
+      content: 'Hello World'
+    },
+    'https://th.wallhaven.cc/small/vq/vqylel.jpg': {
+      content: 'Hello World'
+    },
+    'https://wallhaven.cc/tag/144727': {
+      content: 'Hello World'
+    },
+    'https://wallhaven.cc/search?q=id:144727': {
+      content: 'Hello World'
+    },
+    'https://wallhaven.cc/tag/144726': {
+      content: 'Hello World'
+    },
+    'https://wallhaven.cc/search?q=id:144726': {
       content: 'Hello World'
     },
   }
